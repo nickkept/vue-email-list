d3d9310b1e193d9aee7f8875c38a206402fc6d35
nickkept <nickit1899@gmail.com>
2022-11-30 14:17:21 +0100

    completed project

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            mailList: [],
+            temporaryMailList:[],
+        };
+    },
+    methods: {
+        fetchData() {
+
+            // Creo la richiesta AJAX
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    console.log(resp);
+                    console.log(resp.data.response);
+                    this.temporaryMailList.push(resp.data.response)
+                    if(this.temporaryMailList.length === 10){
+                        this.mailList = this.temporaryMailList;
+                    }
+                    
+                });
+        },
+        getTenMails(){
+            for (let i=0; i<10 ; i++) {
+            
+                this.fetchData();
+            }
+        }
+    },
+    mounted () {
+        
+    }
+}).mount("#app")
+
+
+
+
+
+
+
+
+
+
+
+
+
